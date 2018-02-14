7edeb56e6f8f932f4735bcebe6ddabbddf4e93d4
abdelrahman882 <abdelrahmanahmed882@gmail.com>
2018-02-14 04:54:07 +0200

    got user's info , not tested , almost done

--- app/src/main/java/com/example/android/stadiumsmapapp/LogIn.java
+++ app/src/main/java/com/example/android/stadiumsmapapp/LogIn.java
@@ -2,19 +2,71 @@ package com.example.android.stadiumsmapapp;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.CallbackManager;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import static android.R.attr.x;
+
 
 public class LogIn extends AppCompatActivity {
-
+    CallbackManager callbackManager;
+    LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_log_in);
+        callbackManager=CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+              //  String userId= loginResult.getAccessToken().getUserId();
+                GraphRequest graphRequest=GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            String name = object.getString("first_name");
+                            Toast.makeText(getApplicationContext(),"hello "+name,Toast.LENGTH_LONG).show();
+                        }catch(Exception e){
+
+                        }
+                    }
+                });
+                Bundle b =new Bundle();
+                b.putString("fields","first_name, last_name, email, id");
+                graphRequest.setParameters(b);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(),"canceled ..",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),"an error occurred ..",Toast.LENGTH_LONG).show();
+            }
+        });
         Intent x= new Intent(this,Event.class);
     }
+
     public void guestLogIn(View view)
     {
         Intent x=new Intent(this,Events.class);
@@ -24,5 +76,11 @@ public class LogIn extends AppCompatActivity {
     {
         Intent x=new Intent(this,Posts.class);
         startActivity(x);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
